8ec78ef08b4477ff2003bc7b21aef50989fb5cc9
Yiyang Lin <davidlinyiyang@gmail.com>
2022-02-26 00:02:53 -0600

    sound demo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import * as Tone from 'tone'
 function App() {
+  const synth = new Tone.Synth().toDestination();
+  const mem = new Tone.MembraneSynth().toDestination();
+  const chorus = new Tone.Chorus(4, 2.5, 0.5).toDestination().start();
+  const chorusSynth = new Tone.PolySynth().connect(chorus);
+  
+  const playSynth1 = () => {
+    const now = Tone.now()
+    synth.triggerAttackRelease("C4", "16n", now)
+    synth.triggerAttackRelease("E4", "16n", now + 0.2)
+    synth.triggerAttackRelease("G4", "16n", now + 0.8)
+  }
+  const playSynth2 = () => {
+    mem.triggerAttackRelease("C5", "8n")
+  }
+  const playSynth3 = () => {
+    
+    chorusSynth.triggerAttackRelease(["C3", "E3", "G4"], "8n")
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +27,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={playSynth1}>Play1</button><br/>
+        <button onClick={playSynth2}>Play2</button><br/>
+        <button onClick={playSynth3}>Play3</button>
       </header>
     </div>
   );
