6952b00b445752d0794c40eaabcf3017ddc5955b
SolidifiedRay <yc3346@nyu.edu>
2022-02-27 01:00:03 -0500

    Update Road UI layout

--- client/src/components/Tile.jsx
+++ client/src/components/Tile.jsx
@@ -1,13 +1,13 @@
 export default function Tile({ tileInfo }) {
-  const color = tileInfo['color'];
+  const color = tileInfo["color"];
   return (
     <div>
       <div
         style={{
-          boxShadow: '0px 0px 0px 4px black inset',
-          height: '35px',
-          width: '100px',
-          flexShrink: '0',
+          boxShadow: "0px 0px 0px 4px black inset",
+          height: "35px",
+          width: "100px",
+          flexShrink: "0",
           backgroundColor: color,
         }}
       />
